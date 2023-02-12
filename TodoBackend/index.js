9bb1489832267cbc8f2ba72c68a629223610aca0
Nandlal <shahnandlal49@gmail.com>
2023-02-12 14:49:45 +0530

    added main server page

--- TodoBackend/index.js
+++ TodoBackend/index.js
@@ -1,14 +1,30 @@
- const express=require('express')
- const cors=require('cors');
- const mongoose=require('mongoose');
- const dotenv=require('dotenv');
- 
+const express=require('express')
+const cors=require('cors');
+const mongoose=require('mongoose');
+const todoRoute=require('./routes/todo');
+const dotenv=require('dotenv');
+dotenv.config()
 
 const app = express()
+const PORT= process.env.PORT||8080;
+mongoose.connect()
+.then(()=>{
+    console.log("MongoDB COnnected")
+})
+.catch((err)=>{
+    console.log(err);
+    console.log("OOps unable to connect")
+})
+
+
 
 app.use(express.urlencoded({ extended:true }))
 app.use(express.json())
+app.use(cors());
 
-app.get('/',(req,res)=>res.send('hello'))
+app.get('/',(req,res)=>res.send('Hello! Welcome to The Todo Backend'))
 
-app.listen(8080,()=>{console.log('server started on port 8080')})
+app.get("/todo",todoRoute);
+
+
+app.listen(PORT,()=>{console.log('server started on port 8080')})
